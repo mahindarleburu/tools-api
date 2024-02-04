12bedc6625c302b141f5b77f00bbcbb2c11f26b9
Mahindar Leburu <mahindarleburu@gmail.com>
2024-02-04 20:58:30 +0800

    Added port for DB

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const config = {
   NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
+  PORT: process.env.DB_PORT,
 
   /** DATABASE */
   db: {
